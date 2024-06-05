df74ef2677e3852240083a005ea8ecc107fccd77
ma6692 <140921409+ma6692@users.noreply.github.com>
2024-06-05 14:29:17 +0200

    Restore '6/5/2024, 2:05:51 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,23 +1,18 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         Speed += 1
-    }
-    if (receivedNumber == 2) {
+    } else if (receivedNumber == 2) {
         Speed += -1
-    }
-    if (receivedNumber == 0) {
+    } else if (receivedNumber == 0) {
         Turning = 0
         Speed = 0
         bitbot.stop(BBStopMode.Coast)
         bitbot.ledClear()
-    }
-    if (receivedNumber == 3) {
+    } else if (receivedNumber == 3) {
         Turning += -1
-    }
-    if (receivedNumber == 4) {
+    } else if (receivedNumber == 4) {
         Turning += 1
-    }
-    if (receivedNumber == 5) {
+    } else if (receivedNumber == 5) {
         Turning = 0
         bitbot.setLedColor(0xFFFFFF)
     }
@@ -31,25 +26,22 @@ Turning = 0
 basic.forever(function () {
     Turning = Math.constrain(Turning, -2, 2)
     Speed = Math.constrain(Speed, -2, 2)
-    if (0 < Speed) {
-        if (5 < bitbot.sonar(BBPingUnit.Centimeters)) {
-            bitbot.stop(BBStopMode.Coast)
-            bitbot.go(BBDirection.Forward, 50 * Speed)
-            bitbot.setLedColor(0xFFFFFF)
-        }
+    if (Turning < 0 || Turning > 0) {
         if (Turning > 0) {
-            bitbot.stop(BBStopMode.Coast)
-            bitbot.BBBias(BBRobotDirection.Left, 50 * Turning)
+            bitbot.rotate(BBRobotDirection.Right, 50 * Turning)
             bitbot.setPixelColor(5, 0xFFFFFF)
             bitbot.setPixelColor(11, 0xFFFF00)
         } else {
-            bitbot.stop(BBStopMode.Coast)
-            bitbot.BBBias(BBRobotDirection.Left, 50 * (-1 * Turning))
+            bitbot.rotate(BBRobotDirection.Left, 50 * (-1 * Turning))
             bitbot.setPixelColor(11, 0xFFFFFF)
             bitbot.setPixelColor(5, 0xFFFF00)
         }
+    } else if (0 < Speed) {
+        if (5 < bitbot.sonar(BBPingUnit.Centimeters)) {
+            bitbot.go(BBDirection.Forward, 50 * Speed)
+            bitbot.setLedColor(0xFFFFFF)
+        }
     } else {
-        bitbot.stop(BBStopMode.Coast)
         bitbot.go(BBDirection.Reverse, 50 * (-1 * Speed))
     }
 })
